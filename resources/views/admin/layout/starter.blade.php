a2e353b55338c7fe5938fb47cb50aa550f6403fe
Denis Koroliov <dkor2017@gmail.com>
2020-09-28 17:48:46 +0300

    fixed pathes

--- resources/views/admin/layout/starter.blade.php
+++ resources/views/admin/layout/starter.blade.php
@@ -12,9 +12,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <title>World Watch Trade Admin</title>
 
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="/admin/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="/admin_package/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="admin/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="/admin_package/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -120,10 +120,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="/admin/plugins/jquery/jquery.min.js"></script>
+<script src="/admin_package/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="/admin/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/admin_package/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/admin/dist/js/adminlte.min.js"></script>
+<script src="/admin_package/dist/js/adminlte.min.js"></script>
 </body>
 </html>
