71a4121c1bf6d8b976ab9c763f93103b6e774cae
Denis Koroliov <dkor2017@gmail.com>
2020-09-28 15:41:54 +0300

    fixed admin login route

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::group(['/admin'], function () {
-    Route::get('/login',[\App\Http\Controllers\Admin\AuthController::class,'showLogin']);
+Route::group(['prefix'=>'/admin'], function () {
+    Route::get('/login',[\App\Http\Controllers\Admin\AuthController::class,'showLogin'])->name('adminlogin');
 });
