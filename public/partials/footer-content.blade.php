ec5bca1466dcb0015788f1777e8cb966900ca230
Denis Koroliov <dkor2017@gmail.com>
2020-10-12 00:49:14 +0300

    updated title assets

---
+++ public/partials/footer-content.blade.php
@@ -0,0 +1,98 @@
+<footer class="footer">
+    <div class="sent-wrap">
+        <div class="sent-news">
+            <h2>Рассылка новинок <span>и новых поступлений</span></h2>
+            <div class="social-sent">
+                <a href="#/" class="social-sent_link">
+                    <img src="./images/icons/viber-hover.svg" alt="img">
+                    <img src="./images/icons/viber.svg" alt="img">
+                    <span>
+                        viber
+                    </span>
+                </a>
+                <a href="#/" class="social-sent_link">
+                    <img src="./images/icons/telegram-hover.svg" alt="img">
+                    <img src="./images/icons/telegram.svg" alt="img">
+                    <span>
+                        telegram
+                    </span>
+                </a>
+                <a href="#/" class="social-sent_link">
+                    <img src="./images/icons/whatsapp-hover.svg" alt="img">
+                    <img src="./images/icons/whatsapp.svg" alt="img">
+                    <span>
+                        whatsapp
+                    </span>
+                </a>
+                <a href="#/" class="social-sent_link">
+                    <img src="./images/icons/email-sms-hover.svg" alt="img">
+                    <img src="./images/icons/email-sms.svg" alt="img">
+                    <span>
+                        sms
+                    </span>
+                </a>
+            </div>
+            <div class="search-wrap">
+                <input type="search" name="search" class="footer-search" placeholder="Введите email">
+                <button class="email-push">
+                    <span>Подписаться</span>
+                    <img src="./images/icons/push-email.svg" alt="img">
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="footer-wrap">
+
+
+        <div class="social-cont">
+            <h2>Мы в соцсетях</h2>
+            <div class="social-wrap">
+                <a href="#/" class="social-wrap_link">
+                    <img src="./images/icons/facebock.svg" alt="img">
+                    <img src="./images/icons/facebook-hover.svg" alt="img">
+                </a>
+                <a href="#/" class="social-wrap_link">
+                    <img src="./images/icons/insta.svg" alt="img">
+                    <img src="./images/icons/insta-hover.svg" alt="img">
+                </a>
+                <a href="#/" class="social-wrap_link">
+                    <img src="./images/icons/youtube.svg" alt="img">
+                    <img src="./images/icons/youtube-hover.svg" alt="img">
+                </a>
+                <a href="#/" class="social-wrap_link">
+                    <img src="./images/icons/twiter.svg" alt="img">
+                    <img src="./images/icons/twiter-hover.svg" alt="img">
+                </a>
+            </div>
+            <div class="hot-line">
+                <p>Горячая линия </p>
+                <a href="tel:+49721906693">+49 721 906693</a>
+            </div>
+        </div>
+
+        <div class="application-cont">
+            <h2>Приложение</h2>
+            <div class="store-cont">
+                <div class="stores">
+                    <a href="#/" class="google-play">
+                        <img src="./images/content/google-play.png" alt="google">
+                    </a>
+                    <a href="#/" class="apple-play">
+                        <img src="./images/content/App-store.png" alt="Apple">
+                    </a>
+                </div>
+                <div class="order-advert">
+                    <div class="text-block">
+                        <p>Заказать рекламу на нашем на сайте</p>
+<!--                        <p></p>-->
+                    </div>
+                    <img src="./images/content/reclama-footer.png" alt="img" class="adv-img">
+                </div>
+            </div>
+        </div>
+        <div class="mob-phone">
+            <p>Горячая линия</p>
+            <a href="tel:+49721906693" class="footer-phone">+49 721 906693</a>
+        </div>
+    </div>
+</footer>
---
+++ public/partials/inline-item-cart.blade.php
@@ -0,0 +1,67 @@
+<div class="inline-cart">
+    <div class="vip-label"></div>
+    <div class="favorite-icon"></div>
+    <a href="#/" class="img-wrap">
+        <img src="./images/content/watch-1.png" alt="img">
+    </a>
+    <div class="info-cart">
+        <a href="#/" class="cart-name">Hublot Big Bang 44MM Evolution Steel Ceramic Watch 301.SM.1770.RX</a>
+        <div class="info-wrap">
+            <div class="block">
+                <div class="price">
+                    <div class="new">
+                        1500$
+                    </div>
+                    <div class="old">
+                        1500$
+                    </div>
+                    <div class="rating-cont">
+                        <img src="./images/icons/star-rat.svg" alt="img">
+                        <img src="./images/icons/star-rat.svg" alt="img">
+                        <img src="./images/icons/star-rat.svg" alt="img">
+                        <img src="./images/icons/star-rat.svg" alt="img">
+                        <img src="./images/icons/star-rat.svg" alt="img">
+                    </div>
+                </div>
+            </div>
+
+            <div class="block-grid bord">
+                <div class="item-infos">
+                    <p>Тип механизма: </p>
+                    <span>Кварцевые  </span>
+                </div>
+                <div class="item-infos">
+                    <p>Материал корпуса:</p>
+                    <span>Сталь</span>
+                </div>
+                <div class="item-infos">
+                    <p>Год: </p>
+                    <span>2011</span>
+                </div>
+                <div class="item-infos">
+                    <p>Состояние: </p>
+                    <span>Очень хорошее</span>
+                </div>
+            </div>
+
+            <div class="block-grid">
+                <div class="item-infos">
+                    <p>ИИ номер:</p>
+                    <span>341.VL.4789.LR.1207.POP15</span>
+                </div>
+                <div class="item-infos">
+                    <p>Объем доставки:</p>
+                    <span>С оригинальными документами</span>
+                </div>
+                <div class="item-infos">
+                    <p>Местоположение:</p>
+                    <span>США, Florida, Aventura</span>
+                </div>
+                <div class="item-infos">
+                    <p>Диаметр:</p>
+                    <span>41 mm</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
