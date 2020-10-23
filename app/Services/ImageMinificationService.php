9790d7afd0eaf419f136c478d609d6bfdab9e261
Anton Iskov <antiskov@gmail.com>
2020-10-23 14:47:52 +0300

    codestyle

--- app/Services/ImageMinificationService.php
+++ app/Services/ImageMinificationService.php
@@ -8,26 +8,24 @@ use Intervention\Image\Facades\Image;
 
 class ImageMinificationService
 {
-    public function minify($image_url, array $image_sizes)
+    public function minify(string $image_url, array $image_sizes)
     {
         $sizes = config('image_sizes');
 
-        $image              = Storage::path($image_url);
+        $image        = Storage::path($image_url);
         $image_resize = Image::make($image);
 
         $images_resized = ['original' => $image_url];
 
         foreach ($image_sizes as $size) {
-            $partials_url = explode('/', $image_url);
-            $last_key  = array_key_last($partials_url);
+            $partials_url            = explode('/', $image_url);
+            $last_key                = array_key_last($partials_url);
             $partials_url[$last_key] = $size . '_' . end($partials_url);
-
+            $save_path               = Storage::path(implode('/', $partials_url));
             $image_resize->resize($sizes[$size]['width'], $sizes[$size]['height']);
-            $image_resize->save(Storage::path(implode('/', $partials_url)));
-
-            $images_resized = [$size => implode('/', $partials_url)];
+            $image_resize->save($save_path);
+            $images_resized          = [$size => $save_path];
         }
         return $images_resized;
     }
-
 }
