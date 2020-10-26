715f87805e234d4416b5ad03062d5da45baa3202
Anton Iskov <antiskov@gmail.com>
2020-10-26 16:29:53 +0200

    delete dump from HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use Locale;
 
 class HomeController extends Controller
 {
     public function main()
     {
-        dump(App::getLocale());
         return view('pages.main');
     }
 }
