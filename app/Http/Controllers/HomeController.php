d33b3d260cbd7046fe158eeb59d3a500ceb05407
Anton Iskov <antiskov@gmail.com>
2020-10-27 13:39:49 +0200

    clean

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,8 @@ namespace App\Http\Controllers;
 
 
 use App\Services\ImageMinificationService;
-use App\Mail\WelcomeMail;
 use App\Models\User;
-use App\Services\ImageMinificationService;
-use App\Services\ResetPassword;
 use App\Services\SecurityService;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 
 class HomeController extends Controller
 {
@@ -27,11 +21,12 @@ class HomeController extends Controller
         return view('pages.main');
     }
 
-    public function t2(string $email) {
+    public function t2(string $email)
+    {
         $pass = new SecurityService();
         $user = User::where('email', '=', $email)->first();
         $pass->resetPassword($user);
 
         return view('pages.main');
     }
-
+}
