6ec2fe959dcba3393b7c7fdfb32c4af0cc379c10
Anton Iskov <antiskov@gmail.com>
2020-11-04 10:22:26 +0200

    t

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Advert;
-use App\Models\AdvertImage;
-use App\Services\AdvertsService;
-use Illuminate\Http\Request;
 
 class GoodsController extends Controller
 {
