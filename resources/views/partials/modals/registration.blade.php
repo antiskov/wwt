9f5c454f6c7ce25e8d8a0ebb8c2c9192a62bd149
Anton Iskov <antiskov@gmail.com>
2021-01-11 17:13:14 +0200

    deleted email placeholder

--- resources/views/partials/modals/registration.blade.php
+++ resources/views/partials/modals/registration.blade.php
@@ -4,7 +4,7 @@
         <form id="registration-form" method='post' class="registration" action="{{ url('/register-user') }}">
             @csrf
             <div class="change-input">
-                <input id="reg-form-email" name="email" type="text" placeholder="{{__('messages.modal_registration_email')}}" required>
+                <input id="reg-form-email" name="email" type="text" required>
             </div>
             <input id="reg-pass" name="password" type="password" placeholder="{{__('messages.modal_registration_password')}}" required>
             <input name="repeat-password" type="password" placeholder="{{__('messages.modal_registration_password_again')}}" required>
