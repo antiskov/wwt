19c9362a5fef2cbd1d32f6e3190f445e9a69586e
Anton Iskov <antiskov@gmail.com>
2021-01-15 14:17:59 +0200

    fix step4

--- public/js/sub-steps.js
+++ public/js/sub-steps.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(e){
     //bot-btns && step guard
     (function () {
-        const btns = document.querySelectorAll('[data-step]');
+        let btns = document.querySelectorAll('[data-step]');
         const anchors = document.querySelectorAll('[data-anchor]');
         const tabs = document.querySelectorAll('[data-tab]');
+        const submitBtn = document.querySelector('[data-id-adv]');
         let step = 1;
 
 
@@ -11,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function(e){
             if(step === 1){
                 // ajax();
                 //от тут місце кароч, при переході з 1-о на 2
-                console.log('ajax')
                 return [...inputs].some(input => !input.value)
             }
             else if(step === 2){
@@ -19,11 +19,42 @@ document.addEventListener('DOMContentLoaded', function(e){
             }else if (step === 3){
                 // ajax();
                 //от тут місце кароч, при переході з 3-о на 4
-                console.log('ajax')
+                submitInfoFoStep4();
                 return [...inputs].some(input => !input.value)
             } else{
                 return [...inputs].some(input => !input.value)
             }
+        }
+
+
+        function submitInfoFoStep2(){
+            console.log(submitBtn.dataset.idAdv);
+            $.ajax({
+                data: $('#create_advert').serializeArray(),
+                type: 'post',
+                url:`/submitting/step2/${submitBtn.dataset.idAdv}`,
+                success: function (data) {
+                    console.log(data.output);
+                    $('#step2').html(data.output);
+                }
+            })
+        }
+
+
+
+        function submitInfoFoStep4(){
+            return $.ajax({
+                data: $('#create_advert').serializeArray(),
+                type: 'post',
+                url:`/submitting/step4/${submitBtn.dataset.idAdv}`,
+                success: function (data) {
+                    // $('#step4').empty();
+                    $('#step4').html(data.output);
+                    bindEventListeners();
+                    return true;
+                }
+            })
+
         }
 
         function getOffset(el) {
@@ -47,13 +78,13 @@ document.addEventListener('DOMContentLoaded', function(e){
         anchors.forEach(el => {
             el.addEventListener('click', function () {
                 const idx = parseInt(this.dataset.anchor);
-               if(step === idx){
-                   return
-               }else if(step < idx){
-                   step++
-               }else{
-                   step--
-               }
+                if(step === idx){
+                    return
+                }else if(step < idx){
+                    step++
+                }else{
+                    step--
+                }
             })
         });
 
@@ -64,36 +95,40 @@ document.addEventListener('DOMContentLoaded', function(e){
             anchors[idx - 1].classList.add('active');
         }
 
-        btns.forEach(btn => {
-            btn.onclick = function(){
-                const nextStep = this.dataset.step;
-                const nextTab = document.querySelector(`[data-anchor='${nextStep}']`);
-                if(nextStep >= step){
-                    const inputs = document.querySelector(`[data-tab="${nextStep - 1}"]`)
-                        .querySelectorAll('[data-step-input]');
-                    if(!checkInputs(inputs, step)){
-                        nextTab.classList.remove('disabled');
-                        clearErrors(inputs);
-                        changeTab(nextStep);
-                        window.scroll({top: 0, left: 0, behavior: 'smooth'});
-                        step++
-                    }else{
-                        const el = [...inputs].find(input => !input.value);
-                        if(el){
-                            console.log('second')
-                            const position = getOffset(el).top;
-                            window.scroll({top: (position - 100), left: 0, behavior: 'smooth'});
-                            nextTab.classList.add('disabled');
-                            inputs.forEach(input => {
-                                !input.value && input.classList.add('input-error')
-                            })
+        function bindEventListeners(){
+            document.querySelectorAll('[data-step]').forEach(btn => {
+                btn.onclick = function(){
+                    const nextStep = this.dataset.step;
+                    const nextTab = document.querySelector(`[data-anchor='${nextStep}']`);
+                    if(nextStep >= step){
+                        const inputs = document.querySelector(`[data-tab="${nextStep - 1}"]`)
+                            .querySelectorAll('[data-step-input]');
+                        if(!checkInputs(inputs, step)){
+                            nextTab.classList.remove('disabled');
+                            clearErrors(inputs);
+                            changeTab(nextStep);
+                            window.scroll({top: 0, left: 0, behavior: 'smooth'});
+                            step++
+                        }else{
+                            const el = [...inputs].find(input => !input.value);
+                            if(el){
+                                console.log('second')
+                                const position = getOffset(el).top;
+                                window.scroll({top: (position - 100), left: 0, behavior: 'smooth'});
+                                nextTab.classList.add('disabled');
+                                inputs.forEach(input => {
+                                    !input.value && input.classList.add('input-error')
+                                })
+                            }
                         }
+                    } else{
+                        document.querySelector(`[data-anchor='${nextStep}']`).click();
                     }
-                } else{
-                    document.querySelector(`[data-anchor='${nextStep}']`).click();
                 }
-            }
-        })
+            })
+        }
+
+        bindEventListeners();
     })()
 
 });
