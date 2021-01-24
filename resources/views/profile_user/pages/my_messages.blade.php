c85b17b41f7698803edcc7c46edb6d92c091f302
Denis Koroliov <dkor2017@gmail.com>
2021-01-25 01:08:36 +0200

    left menu to messages

--- resources/views/profile_user/pages/my_messages.blade.php
+++ resources/views/profile_user/pages/my_messages.blade.php
@@ -4,27 +4,7 @@
     <section class="messages">
         <div class="">
             <div class="container-wrap">
-                <div class="bg-cont">
-                    <div class="person-block">
-                        <img src="./images/icons/close.svg" alt="img" class="close">
-                        <div class="person-info">
-                            <div class="img-cont">
-                                <img src="./images/content/person.png" alt="img">
-                            </div>
-                            <div class="peron-name">LUXE WARE (HK) LIMITED</div>
-                        </div>
-                        <ul class="link-items">
-                            <li><img src="./images/icons/my-order.svg" alt=""><a href="#/">Мои объявления</a></li>
-                            <li><img src="./images/icons/email.svg" alt=""><a href="#/">Сообщения</a></li>
-                            <li><img src="./images/icons/pay.svg" alt=""><a href="#/">Платежи</a></li>
-                            <li><img src="./images/icons/profile.svg" alt=""><a href="#/">Профиль</a></li>
-                            <li><img src="./images/icons/setting.svg" alt=""><a href="#/">Настройки</a></li>
-                            <li><img src="./images/icons/favor.svg" alt=""><a href="#/">Избранное</a></li>
-                            <li><img src="./images/icons/ref-link.svg" alt=""><a href="#/">Создать реферальную ссылку</a></li>
-                            <li><img src="./images/icons/exit.svg" alt=""><a href="#/">Выход</a></li>
-                        </ul>
-                    </div>
-                </div>
+                @include('profile_user.partials.left_menu')
 
                 <div class="block-messages">
                     <h2 class="name-lk">Сообщения</h2>
