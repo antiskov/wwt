74952e2c991d1606e4f672e07749ca977b028323
Denis Koroliov <dkor2017@gmail.com>
2021-01-25 03:26:58 +0200

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-Vue.component('chat', require('./components/Chat'))
+Vue.component('chat', require('./components/Chat').deafult)
 
 const app=new Vue({
     el: "#app"
