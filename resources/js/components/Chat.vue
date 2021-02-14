b07679b8d356eba36ea8ecfcf9c220478ee572fd
Denis Koroliov <dkor2017@gmail.com>
2021-02-14 19:55:11 +0200

    t

--- resources/js/components/Chat.vue
+++ resources/js/components/Chat.vue
@@ -69,6 +69,7 @@
 
                 const m = await axios.post('/dialog/'+this.dialog_id+'/messages', d);
                 const mes=JSON.parse(m.config.data)
+                console.log(m.data)
                 console.log(mes);
                 this.messages.push(mes);
                 console.log(this.messages)
