c8752c02bbef531cd72545872ad747439def3952
Denis Koroliov <dkor2017@gmail.com>
2021-02-14 18:25:18 +0200

    remove_die

--- app/Http/Controllers/DialogsController.php
+++ app/Http/Controllers/DialogsController.php
@@ -34,9 +34,6 @@ class DialogsController extends Controller
     public function sendMessage(Request $request)
     {
         Log::info($request);
-        die();
-
-
 
         $m=new Messages();
         $m->dialog_id=$request->dialog_id;
