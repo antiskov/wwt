fb6e1747783013cb59b8bdf11b774e7c0eeae4b6
Denis Koroliov <dkor2017@gmail.com>
2021-02-14 18:55:04 +0200

    fixed modules

--- resources/js/components/Chat.vue
+++ resources/js/components/Chat.vue
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="communication-items">
             <div v-for="message in messages" :key="message.text" class="communication-items-in" :class="checkClass(message)" >
-                <img src="./images/content/person.png" alt="img">
+                <img src="/images/content/person.png" alt="img">
                 <div class="text-mas-in">
                     <p>
                         {{ message.text }}
                     </p>
                     <div class="time">18.06</div>
-<img class="label-del" src="images/icons/ok-white.svg">
+<img class="label-del" src="/images/icons/ok-white.svg">
                 </div>
             </div>
             <div class="communication-items-out">
@@ -18,7 +18,7 @@
                         Вы купили эти часы? Я не покупал эти часы
                     </p>
                     <div class="time">18.06</div>
-<img class="label-del" src="./images/icons/ok-white.svg">
+<img class="label-del" src="/images/icons/ok-white.svg">
                 </div>
             </div>-->
         </div>
