f5b74e4cf0c3fa1976134b6df2c08396b3da77b4
Denis Koroliov <dkor2017@gmail.com>
2021-02-14 19:02:29 +0200

    add w

--- resources/js/components/Chat.vue
+++ resources/js/components/Chat.vue
@@ -1,26 +1,16 @@
 <template>
     <div class="container">
         <div class="communication-items">
-            <div v-for="message in messages" :key="message.text" class="communication-items-in" :class="checkClass(message)" >
+            <div v-for="message in messages" :key="message.text" :class="checkClass(message)" >
                 <img src="/images/content/person.png" alt="img">
                 <div class="text-mas-in">
                     <p>
                         {{ message.text }}
                     </p>
                     <div class="time">18.06</div>
-<img class="label-del" src="/images/icons/ok-white.svg">
+                    <img class="label-del" src="/images/icons/ok-white.svg">
                 </div>
             </div>
-            <div class="communication-items-out">
-<img src="/images/content/person.png" alt="img">
-                <div class="text-mas-in">
-                    <p>
-                        Вы купили эти часы? Я не покупал эти часы
-                    </p>
-                    <div class="time">18.06</div>
-<img class="label-del" src="/images/icons/ok-white.svg">
-                </div>
-            </div>-->
         </div>
         <div class="cont">
             <textarea class="communication-mess" ref="messageField" placeholder="Введите текст" v-model="textMessage"></textarea>
