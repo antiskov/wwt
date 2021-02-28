54579dc6d74593236fb2921af927443c0917e687
Denis Koroliov <dkor2017@gmail.com>
2021-02-28 19:38:15 +0200

    date

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,8 @@ Vue.component('chat', require('./components/Chat').default)
 
 Vue.filter('formatDate', function(value) {
     if (value) {
-        return moment(String(value)).format('MM/DD/YYYY hh:mm:ss')
+        var msUTC = Date.parse(value);
+        return dateFormat(msUTC, 'MM/DD/YYYY hh:mm:ss');
     }
 })
 
