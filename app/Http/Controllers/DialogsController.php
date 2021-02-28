5098a68a3e1e98e614de0cb430275c2e2c6b79db
Denis Koroliov <dkor2017@gmail.com>
2021-02-28 19:10:02 +0200

    t

--- app/Http/Controllers/DialogsController.php
+++ app/Http/Controllers/DialogsController.php
@@ -32,6 +32,11 @@ class DialogsController extends Controller
         $respondent_avatar=$currentDialog?(new \App\Services\ProfileService())->getAvatar($currentDialog->advert->user_id):'/images/content/person.png';
         $ua=(new \App\Services\ProfileService())->getAvatar(Auth::id());
         $user_avatar=$ua?$ua:'/images/icons/wwt_profile_avatar.png';
+        if (Auth::id()==$currentDialog->advert->user_id) {
+            $temp=$respondent_avatar;
+            $respondent_avatar=$user_avatar;
+            $user_avatar=$temp;
+        }
         return view('profile_user.pages.my_messages',
         [
             'dialogs'=>$dialogs,
